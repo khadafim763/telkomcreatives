f4af68442e6213fe9b1b4140ce8f1dcf9a44a466
khadafim763 <48129809+khadafim763@users.noreply.github.com>
2020-10-30 16:53:08 +0700

    Update index.php

--- charge/index.php
+++ charge/index.php
@@ -1,6 +1,6 @@
 <?php
 // Set your server key (Note: Server key for sandbox and production mode are different)
-$server_key = '<server key>';
+$server_key = 'SB-Mid-server-UeqURFJhzgKlss1g9VYPpyDZ';
 // Set true for production, set false for sandbox
 $is_production = false;
 
